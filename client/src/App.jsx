3a3d6ec7ef0dfaaa94381aa84ef182da18c10619
salih-kv <profileofsalih@gmail.com>
2023-11-01 20:02:34 +0530

    create: home & create post pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
+import { CreatePost, Home } from "./pages";
 import { Header } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] ">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
